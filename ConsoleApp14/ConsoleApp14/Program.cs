f32409ede1b93881f3e2041b4ac52c28f3418c6d
Teodor <teodorbear26@gmail.com>
2018-11-13 17:45:04 +0200

    Some kind of changes.

--- ConsoleApp14/ConsoleApp14/Program.cs
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -13,6 +13,9 @@ namespace ConsoleApp14
     {
         public static void Main(string[] args)
         {
+            File.Delete(Path);
+            File.Delete(Path1);
+            File.Delete(Path2);
 
             Person jim = new Person();
             jim.Name = "Jim";
@@ -29,63 +32,67 @@ namespace ConsoleApp14
             cat.Legs = 4;
             cat.Height = 50;
 
+            SetAllProp<Person>(jim);
+            SetAllProp<Car>(tesla);
+            SetAllProp<Animal>(cat);
 
-            GetAllProp<Person>(jim);
-            GetAllProp<Car>(tesla);
-            GetAllProp<Animal>(cat);
-
-            GetGromFile<Person>(jim);
-            GetGromFile<Car>(tesla);
-            GetGromFile<Animal>(cat);
+            SetFromFile<Person>(jim);
+            SetFromFile<Car>(tesla);
+            SetFromFile<Animal>(cat);
+          
             Console.ReadLine();
 
+            
 
         }
+
+        
+
         public static string Path = @"C:\Homework\Person.txt";
         public static string Path1 = @"C:\Homework\Animal.txt";
         public static string Path2 = @"C:\Homework\Car.txt";
 
 
-        public static void GetAllProp<T>(T obj)
+        public static void SetAllProp<T>(T obj)
         {
             var className = nameof(T);
             var objpath = $"{Path}{className}.txt";
+
             foreach (var prop in obj.GetType().GetProperties())
             {
                 var value = prop.GetValue(obj, null);
-                string str = $"{prop.Name}: {value} ";
+                string str = $" {prop.Name}: {value} ";
                 str = str + Environment.NewLine;
+
                 if (obj.GetType() == typeof(Person))
                     File.AppendAllText(Path, str);
-                else if (obj.GetType() == typeof(Animal))
-                {
+
+                else if (obj.GetType() == typeof(Animal))                
                     File.AppendAllText(Path1, str);
-                }
-                else if (obj.GetType() == typeof(Car))
-                {
-                    File.AppendAllText(Path2, str);
-                }
+                
+                else  File.AppendAllText(Path2, str);
+                
             }           
         }
-        public static void GetGromFile<T>(T obj)
+        public static void SetFromFile<T>(T obj)
         {
             if (obj.GetType() == typeof(Person))
             {
                 string text = File.ReadAllText(@"C:\Homework\Person.txt", Encoding.UTF8);                
                 Console.WriteLine("Текст из файла: {0}", text);
-                File.Create(Path).Close();
+               
             }
             else if (obj.GetType() == typeof(Animal))
             {
                 string text = File.ReadAllText(@"C:\Homework\Animal.txt", Encoding.UTF8);
                 Console.WriteLine("Текст из файла: {0}", text);
-                File.Create(Path1).Close();
+               
             }
             else if (obj.GetType() == typeof(Car))
             {
                 string text = File.ReadAllText(@"C:\Homework\Car.txt", Encoding.UTF8);
                 Console.WriteLine("Текст из файла: {0}", text);
-                File.Create(Path2).Close();
+                
             }
 
         }
